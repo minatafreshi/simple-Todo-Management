f1ad10a511939ff7597cb8eae938088dbe3e230a
mina tafreshi <info.minatafreshi@gmail.com>
2020-09-02 11:56:39 +0430

    create model part

---
+++ src/main/java/net/guides/springboot/todomanagement/model/Todo.java
@@ -0,0 +1,60 @@
+package net.guides.springboot.todomanagement.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "todos")
+
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String userName;
+    private String description;
+    private Date targetDate;
+
+    public Todo() {
+        super();
+    }
+
+    public Todo(String userName, String description, Date targetDate, Boolean isDone) {
+        super();
+        this.userName = userName;
+        this.description = description;
+        this.targetDate = targetDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(Date targetDate) {
+        this.targetDate = targetDate;
+    }
+}
